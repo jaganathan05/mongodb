2b86c3cf4e9174a886d1ebcfc065e0815f9a82f2
jaganathan05 <jaganathanv888@gmail.com>
2024-01-19 15:58:00 +0530

    Getting Orders Placed

--- models/user.js
+++ models/user.js
@@ -64,6 +64,29 @@ class User {
     const db=getdb();
     return db.collection('users').updateOne({ _id: new ObjectId(this._id)},{$set:{cart: {items: updatedCartItems}}})
   }
+  getOrder(){
+    const db = getdb();
+    return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray()
+  }
+
+  addOrder(){
+    const db = getdb()
+    return this.getCart().then(products=>{
+      const order = {
+        items: products,
+        user: {
+          _id : new ObjectId(this._id),
+          name: this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart = { items:[]};
+
+      return db.collection('users').updateOne({ _id: new ObjectId(this._id)},{$set:{cart: {items: []}}})
+    })
+  }
 }
 
 
