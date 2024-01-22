aaae67309194dd1d631b67fec428ba4cb7261f24
jaganathan05 <jaganathanv888@gmail.com>
2024-01-22 11:03:54 +0530

    Cart System

--- models/user.js
+++ models/user.js
@@ -22,6 +22,28 @@ const userSchema = new Schema ({
     }
 })
 
+userSchema.methods.addToCart = function(product){
+            const cartProductIndex = this.cart.items.findIndex(cp=>{
+              return cp.productId.toString() === product._id.toString();
+            })
+            let newQuentity = 1;
+            const updatedCartItems = [...this.cart.items];
+        
+            if(cartProductIndex >=0 ){
+              newQuentity= this.cart.items[cartProductIndex].quantity + 1
+              updatedCartItems[cartProductIndex].quantity = newQuentity;
+            }
+            else{
+              updatedCartItems.push({productId: product._id, quantity : newQuentity})
+            }
+            
+            const updatedCart = { items:updatedCartItems}
+        
+
+            this.cart=updatedCart;
+            return this.save()
+}
+
 module.exports = mongoose.model('User',userSchema);
 
 // const getdb = require('../util/database').getdb;
